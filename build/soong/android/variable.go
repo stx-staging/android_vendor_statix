RevengeOS/android_vendor_revengeos

vendor: Rewrite soong_config for Pie
Change-Id: I71ba2262026dda8688faeff712f57aa6c001c656

build/soong/android/variable.go
@@ -21,7 +21,7 @@ type Product_variables struct {
 	}
 
 	Qcom_bsp_legacy struct {
-		Cflags []string
+		Cppflags []string
 	}
 
 	Qti_flac_decoder struct {
@@ -50,19 +50,19 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	Mtk_hardware            *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
-	Qcom_bsp_legacy         *bool `json:",omitempty"`
-	Qti_flac_decoder        *bool `json:",omitempty"`
-	Target_uses_qsml      *bool `json:",omitempty"`
-	Target_uses_eigen       *bool `json:",omitempty"`
-	Target_shim_libs  *string `json:",omitempty"`
+	Has_legacy_camera_hal1						*bool `json:",omitempty"`
+	Uses_media_extensions						*bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
+	Specific_camera_parameter_library			*string `json:",omitempty"`
+	Needs_text_relocations						*bool `json:",omitempty"`
+	Mtk_hardware								*bool `json:",omitempty"`
+	QTIAudioPath								*string `json:",omitempty"`
+	QTIDisplayPath								*string `json:",omitempty"`
+	QTIMediaPath								*string `json:",omitempty"`
+	Cant_reallocate_omx_buffers					*bool `json:",omitempty"`
+	Qcom_bsp_legacy								*bool `json:",omitempty"`
+	Qti_flac_decoder							*bool `json:",omitempty"`
+	Target_uses_qsml							*bool `json:",omitempty"`
+	Target_uses_eigen							*bool `json:",omitempty"`
+	Target_shim_libs							*string `json:",omitempty"`
 }

build/soong/soong_config.mk
@@ -1,25 +1,23 @@
-carbon_soong:
-	$(hide) mkdir -p $(dir $@)
-	$(hide) (\
-	echo '{'; \
-	echo '"Carbon": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "Mtk_hardware": $(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false),'; \
-	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),';  \
-	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
-	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
-	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
-	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
-	echo '    "Cant_reallocate_omx_buffers":  $(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false),';  \
-	echo '    "Qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),';  \
-	echo '    "Qti_flac_decoder":  $(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false),';  \
-	echo '    "TargetUsesProprietaryLibs":  $(if $(strip $(TARGET_USES_PROPRIETARY_LIBS)),true,false),';  \
-	echo '    "Target_uses_qsml":  $(if $(strip $(TARGET_USES_QSML)),true,false),';  \
-	echo '    "Target_uses_eigen":  $(if $(strip $(TARGET_USES_QSML)),false,true),';  \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
-	echo '},'; \
-	echo '') > $(SOONG_VARIABLES_TMP)
+$(eval _contents := $$(_contents)"Carbon": {$$(newline))
+
+$(call add_json_bool,	Has_legacy_camera_hal1,						$(filter true,$(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false)))
+$(call add_json_bool,	Uses_media_extensions,						$(filter true,$(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false)))
+$(call add_json_bool,	Uses_generic_camera_parameter_library		$(filter true,$(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true)))
+$(call add_json_bool,	Needs_text_relocations,						$(filter true,$(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)))
+$(call add_json_bool,	Mtk_hardware,								$(filter true,$(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false)))
+$(call add_json_bool,	BoardUsesQTIHardware,						$(filter true,$(if $(BOARD_USES_QTI_HARDWARE),true,false)))
+$(call add_json_bool,	Cant_reallocate_omx_buffers,				$(filter true,$(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false)))
+$(call add_json_bool,	Qcom_bsp_legacy,							$(filter true,$(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false)))
+$(call add_json_bool,	Qti_flac_decoder,							$(filter true,$(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false)))
+$(call add_json_bool,	TargetUsesProprietaryLibs,					$(filter true,$(if $(strip $(TARGET_USES_PROPRIETARY_LIBS)),true,false)))
+$(call add_json_bool,	Target_uses_qsml,							$(filter true,$(if $(strip $(TARGET_USES_QSML)),true,false)))
+$(call add_json_bool,	Target_uses_eigen,							$(filter true,$(if $(strip $(TARGET_USES_QSML)),false,true)))
+
+$(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str,	Libart_img_base,							$(LIBART_IMG_BASE))
+$(call add_json_str,	QTIAudioPath,								$(call project-path-for,qcom-audio))
+$(call add_json_str,	QTIDisplayPath,								$(call project-path-for,qcom-display))
+$(call add_json_str,	QTIMediaPath,								$(call project-path-for,qcom-media))
+$(call add_json_str,	Target_shim_libs,							$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+
+$(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))

stx-staging/android_vendor_statix

build/soong/android/variable.go
@@ -25,7 +25,7 @@ type Product_variables struct {
 	}
 
 	Qcom_bsp_legacy struct {
-		Cflags []string
+		Cppflags []string
 	}
 
 	Qti_flac_decoder struct {
@@ -41,18 +41,18 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
-	Has_legacy_camera_hal1  *bool `json:",omitempty"`
-	Needs_legacy_camera_hal1_dyn_native_handle  *bool `json:",omitempty"`
-	Uses_media_extensions   *bool `json:",omitempty"`
-	Uses_generic_camera_parameter_library  *bool `json:",omitempty"`
-	Specific_camera_parameter_library  *string `json:",omitempty"`
-	Needs_text_relocations  *bool `json:",omitempty"`
-	Mtk_hardware            *bool `json:",omitempty"`
-	QTIAudioPath            *string `json:",omitempty"`
-	QTIDisplayPath          *string `json:",omitempty"`
-	QTIMediaPath            *string `json:",omitempty"`
-	Cant_reallocate_omx_buffers *bool `json:",omitempty"`
-	Qcom_bsp_legacy         *bool `json:",omitempty"`
-	Qti_flac_decoder        *bool `json:",omitempty"`
-	Target_shim_libs  *string `json:",omitempty"`
+	Has_legacy_camera_hal1						*bool `json:",omitempty"`
+	Needs_legacy_camera_hal1_dyn_native_handle	*bool `json:",omitempty"`
+	Uses_media_extensions						*bool `json:",omitempty"`
+	Uses_generic_camera_parameter_library		*bool `json:",omitempty"`
+	Specific_camera_parameter_library			*string `json:",omitempty"`
+	Needs_text_relocations						*bool `json:",omitempty"`
+	Mtk_hardware								*bool `json:",omitempty"`
+	QTIAudioPath								*string `json:",omitempty"`
+	QTIDisplayPath								*string `json:",omitempty"`
+	QTIMediaPath								*string `json:",omitempty"`
+	Cant_reallocate_omx_buffers					*bool `json:",omitempty"`
+	Qcom_bsp_legacy								*bool `json:",omitempty"`
+	Qti_flac_decoder							*bool `json:",omitempty"`
+	Target_shim_libs							*string `json:",omitempty"`
 }

build/soong/soong_config.mk
@@ -1,23 +1,21 @@
-statix_soong:
-	$(hide) mkdir -p $(dir $@)
-	$(hide) (\
-	echo '{'; \
-	echo '"Statix": {'; \
-	echo '    "Has_legacy_camera_hal1": $(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false),'; \
-	echo '    "Needs_legacy_camera_hal1_dyn_native_handle": $(if $(filter true,$(TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE)),true,false),'; \
-	echo '    "Uses_media_extensions": $(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false),'; \
-	echo '    "Uses_generic_camera_parameter_library": $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true),'; \
-	echo '    "Specific_camera_parameter_library": "$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)",'; \
-	echo '    "Needs_text_relocations": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false),'; \
-	echo '    "Mtk_hardware": $(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false),'; \
-	echo '    "BoardUsesQTIHardware":  $(if $(BOARD_USES_QTI_HARDWARE),true,false),';  \
-	echo '    "Libart_img_base": "$(LIBART_IMG_BASE)",'; \
-	echo '    "QTIAudioPath":  "$(call project-path-for,qcom-audio)",'; \
-	echo '    "QTIDisplayPath":  "$(call project-path-for,qcom-display)",'; \
-	echo '    "QTIMediaPath":  "$(call project-path-for,qcom-media)",';  \
-	echo '    "Cant_reallocate_omx_buffers":  $(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false),';  \
-	echo '    "Qcom_bsp_legacy":  $(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false),';  \
-	echo '    "Qti_flac_decoder":  $(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false),';  \
-	echo '    "Target_shim_libs": "$(subst $(space),:,$(TARGET_LD_SHIM_LIBS))"'; \
-	echo '},'; \
-	echo '') > $(SOONG_VARIABLES_TMP)
+$(eval _contents := $$(_contents)"Statix": {$$(newline))
+
+$(call add_json_bool,	Has_legacy_camera_hal1,						$(filter true,$(if $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)),true,false)))
+$(call add_json_bool,	Needs_legacy_camera_hal1_dyn_native_handle,	$(filter true,$(if $(filter true,$(TARGET_NEEDS_LEGACY_CAMERA_HAL1_DYN_NATIVE_HANDLE)),true,false)))
+$(call add_json_bool,	Uses_media_extensions,						$(filter true,$(if $(filter true,$(TARGET_USES_MEDIA_EXTENSIONS)),true,false)))
+$(call add_json_bool,	Uses_generic_camera_parameter_library		$(filter true,$(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true)))
+$(call add_json_bool,	Needs_text_relocations,						$(filter true,$(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXT_RELOCATIONS)),true,false)))
+$(call add_json_bool,	Mtk_hardware,								$(filter true,$(if $(filter true,$(BOARD_USES_MTK_HARDWARE)),true,false)))
+$(call add_json_bool,	BoardUsesQTIHardware,						$(filter true,$(if $(BOARD_USES_QTI_HARDWARE),true,false)))
+$(call add_json_bool,	Cant_reallocate_omx_buffers,				$(filter true,$(if $(filter omap4,$(TARGET_BOARD_PLATFORM)),true,false)))
+$(call add_json_bool,	Qcom_bsp_legacy,							$(filter true,$(if $(filter msm7x27a msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),true,false)))
+$(call add_json_bool,	Qti_flac_decoder,							$(filter true,$(if $(strip $(AUDIO_FEATURE_ENABLED_EXTN_FLAC_DECODER)),true,false)))
+
+$(call add_json_str,	Specific_camera_parameter_library,			$(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY))
+$(call add_json_str,	Libart_img_base,							$(LIBART_IMG_BASE))
+$(call add_json_str,	QTIAudioPath,								$(call project-path-for,qcom-audio))
+$(call add_json_str,	QTIDisplayPath,								$(call project-path-for,qcom-display))
+$(call add_json_str,	QTIMediaPath,								$(call project-path-for,qcom-media))
+$(call add_json_str,	Target_shim_libs,							$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+
+$(eval _contents := $(subst $$(comma)$$(newline)__SV_END,$(newline),$$(_contents)__SV_END},$$(newline)))