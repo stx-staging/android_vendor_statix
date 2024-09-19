15b8a801ded623dda2cca7f108c87fd1975b3a21
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2024-09-19 09:49:46 -0400

    statix: vendorsetup: Make it useful for builders
    Change-Id: I7d122d16cf7c4e7e18b4f2bb2902fd744854dd13
    Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/14779
    Reviewed-by: Ralph Chen <ralph950412@gmail.com>
    Reviewed-by: Sourajit Karmakar <sourajit@live.com>
    Tested-by: Sourajit Karmakar <sourajit@live.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,24 @@
-devices=( 'cannon' 'guacamoleb' 'lemonadep' 'mata' 'oriole' 'sake' 'sargo' 'raven' 'TP1803')
+#!/bin/bash
 
-function lunch_devices() {
-    add_lunch_combo statix_${device}-user
-    add_lunch_combo statix_${device}-userdebug
-}
+# Color variables
+YELLOW='\033[1;33m'
+RESET='\033[0m'
 
-for device in ${devices[@]}; do
-    lunch_devices
-done
+# Welcome message
+echo -e "Welcome to ${YELLOW}StatiX!${RESET}"
+
+# Additional instructions
+echo -e "\nHere are some things before you get started!\n"
+echo -e "- Make sure you have these variables highlighted in this link in your device tree:"
+echo -e "  https://github.com/StatiXOS/android_device_xiaomi_sky/blob/udc/device.mk#L11-L14"
+
+echo -e "\n- Next, make sure you have a prebuilt kernel repo set up. It should look something like this:"
+echo -e "  https://github.com/StatiXOS/android_device_xiaomi_sky-kernel"
+
+echo -e "\n- If you don't have a prebuilt kernel repo, follow the manifests we have here for other devices and adapt it to yours:"
+echo -e "  https://github.com/StatiXOS/android_kernel_manifest"
+
+echo -e "\n- Finally, push any changes to our Gerrit if you think they're worthwhile, and we will try to review it as soon as possible.\n  Follow the guide below to get set up with our Gerrit:"
+echo -e "  https://github.com/StatiXOS/android_manifest?tab=readme-ov-file#submitting-patches"
+
+echo -e "\nDon't forget to join https://t.me/StatiXOSReleases! Enjoy! :D"
