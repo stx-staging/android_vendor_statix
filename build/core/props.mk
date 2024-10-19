92399e99a347f859968a907a097e23eb48ea65fe
Michael Bestas <mkbestas@gmail.com>
2024-10-19 18:09:38 +0000

    build: Move version properties to PRODUCT_SYSTEM_PROPERTIES
    ADDITIONAL_SYSTEM_PROPERTIES is being deprecated.
    No functional changes.
    
    Change-Id: I01fd45edcc6c700540d3b7352fc466e65677a44e
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- build/core/props.mk
+++ build/core/props.mk
@@ -3,11 +3,3 @@ ifneq ($(BUILD_FINGERPRINT),)
 ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
-
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.statix.version=$(STATIX_BASE_VERSION)-$(STATIX_BUILD_TYPE)-$(BUILD_DATE) \
-    ro.statix.base.version=$(STATIX_BASE_VERSION) \
-    ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
-    ro.statix.fingerprint=$(ROM_FINGERPRINT) \
-    ro.statix.buildtype=$(STATIX_BUILD_TYPE) \
-    ro.statix.device=$(TARGET_DEVICE)
