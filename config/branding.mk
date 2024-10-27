6ad159cd94b92b8afef092151490d134298eef87
ralph950412 <ralph950412@gmail.com>
2024-10-27 17:00:05 +0000

    statix: Drop ro.statix.device declaration
    This will be set via gen_build_prop.py instead.
    
    Change-Id: Ia28349c0b1db8c375e16efe5171724acbb31b308

--- config/branding.mk
+++ config/branding.mk
@@ -29,8 +29,7 @@ PRODUCT_SYSTEM_PROPERTIES += \
     ro.statix.base.version=$(STATIX_BASE_VERSION) \
     ro.mod.version=$(BUILD_ID)-$(BUILD_DATE)-$(STATIX_BASE_VERSION) \
     ro.statix.fingerprint=$(ROM_FINGERPRINT) \
-    ro.statix.buildtype=$(STATIX_BUILD_TYPE) \
-    ro.statix.device=$(TARGET_DEVICE)
+    ro.statix.buildtype=$(STATIX_BUILD_TYPE)
 
 ## Signing
 ifneq (eng,$(TARGET_BUILD_VARIANT))
