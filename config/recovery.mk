1a705e2ce8af03a0fa17ba8bb02a590965d43ddd
Vaisakh Murali <mvaisakh@statixos.com>
2026-06-16 15:11:30 +0800

    statix: Export recovery ui lib via soong config
    Change-Id: Ibb8f0ad343cf3dfb2f5c81a3dadcf7f136d1b324
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: ralph950412 <ralph950412@gmail.com>

--- config/recovery.mk
+++ config/recovery.mk
@@ -3,3 +3,8 @@
 
 # Recovery UI
 TARGET_RECOVERY_UI_LIB := librecovery_ui_statix
+
+# The Make-to-Soong bridge
+SOONG_CONFIG_NAMESPACES += recovery
+SOONG_CONFIG_recovery += target_recovery_ui_lib
+SOONG_CONFIG_recovery_target_recovery_ui_lib := librecovery_ui_statix
